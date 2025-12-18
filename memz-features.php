a47bc7de08d51b2289c504dc9b0e770ae69fbd64
Manuel esteban Morales Zuarez <manuesteban1990@gmail.com>
2025-12-18 13:13:48 -0400

    first commit

---
+++ memz-features.php
@@ -0,0 +1,19 @@
+<?php 
+
+/**
+ * PLUGIN NAME: WP PRACTICE
+ * VERSION: 1.0.0
+ * REQUIRES PHP: 7.2
+ * REQUIERES AT LEAST: 5.0
+ * AUTHOR: Manuel Morales
+ * AUTHOR URI: https://github.com/Angstromico
+ * 
+ * TEXT DOMAIN: PRACTICE
+ * LICENSE: GPL v2 or later
+ * LICENSE URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * DESCRIPTION: This is the plugin for the practice of WP
+ * DOMAIN PATH: /languages
+ * PLUGIN URI: git@github.com:Angstromico/Practice-WordPress-Pluging.git
+ */
+
+if ( ! defined( 'ABSPATH' ) ) { exit; }
